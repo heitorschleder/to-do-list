db3e0d288f5492bad3da5b9300f9b1e8744fe806
Heitor Schleder <100155822+heitorschleder@users.noreply.github.com>
2025-02-18 19:42:01 -0300

    Update Footer.vue in logo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
     <footer class="footer fixed-bottom d-flex justify-content-center align-items-center p-3">
         <p class="mb-0 me-2">Uma parceria de Heitor Schleder com</p>
-        <img lazyload src="https://www.effecti.com.br/wp-content/uploads/2023/07/logotipo.svg" alt="effecti"
-            class="img-fluid" />
+        <img lazyload src="https://miro.medium.com/v2/resize:fit:700/1*qc9iypKE2wJrvM99sUZF4Q.png" alt="effecti"
+            class="img-fluid logo-att" />
     </footer>
 </template>
 
@@ -13,6 +13,9 @@ export default {
 </script>
 
 <style scoped>
+.logo-att {
+    height: 100px;
+}
 .footer {
     color: azure;
     background-color: #002352;
